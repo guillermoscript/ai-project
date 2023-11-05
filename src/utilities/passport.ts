a91799bd25c91bb095e77088d97c21b4ecba3ebf
guillermoscript <arepayquezo@gmail.com>
2023-11-05 12:05:37 -0400

    Commented out passport and Google OAuth imports and configurations

--- src/utilities/passport.ts
+++ src/utilities/passport.ts
@@ -1,34 +1,34 @@
-import passport from 'passport'
-import { Strategy as GoogleStrategy } from 'passport-google-oauth2'
+// import passport from 'passport'
+// import { Strategy as GoogleStrategy } from 'passport-google-oauth2'
 
-require('dotenv').config();
+// require('dotenv').config();
 
-passport.serializeUser(function (user, done) {
-	done(null, user);
-});
+// passport.serializeUser(function (user, done) {
+// 	done(null, user);
+// });
 
-passport.deserializeUser(function (user: any, done) {
-	done(null, user);
-});
+// passport.deserializeUser(function (user: any, done) {
+// 	done(null, user);
+// });
 
-if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
-	throw new Error('Missing Google OAuth Client ID and/or Client Secret');
-}
+// if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+// 	throw new Error('Missing Google OAuth Client ID and/or Client Secret');
+// }
 
-passport.use(new GoogleStrategy({
-	clientID: process.env.CLIENT_ID || '',
-	clientSecret: process.env.CLIENT_SECRET || '',
-	callbackURL: process.env.SERVER_URL + '/oauth2/callback',
-	passReqToCallback: true
-},
+// passport.use(new GoogleStrategy({
+// 	clientID: process.env.CLIENT_ID || '',
+// 	clientSecret: process.env.CLIENT_SECRET || '',
+// 	callbackURL: process.env.PAYLOAD_PUBLIC_SERVER_URL + '/oauth2/callback',
+// 	passReqToCallback: true
+// },
 
-	function (request: any, accessToken: string, refreshToken: any, profile: any, done: any) {
-		return done(null, {
-			accessToken,
-			refreshToken,
-			profile
-		});
-	}
-));
+// 	function (request: any, accessToken: string, refreshToken: any, profile: any, done: any) {
+// 		return done(null, {
+// 			accessToken,
+// 			refreshToken,
+// 			profile
+// 		});
+// 	}
+// ));
 
-export default passport
+// export default passport
